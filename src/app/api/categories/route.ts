b80ed9a0b0ef785e946ce3bff6e9ba48117920ce
hammer0x419 <hammer@forgeai.gg>
2025-02-06 05:52:50 -0800

    feat: add optional type param

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -3,7 +3,19 @@ import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   try {
+    const searchParams = request.nextUrl.searchParams;
+    const type = searchParams.get("type");
     const categories = await generateCategories();
+
+    if (type) {
+      return new Response(JSON.stringify(categories[type]), {
+        headers: {
+          "Content-Type": "application/json",
+          "Cache-Control": "public, max-age=31536000, immutable",
+          Vary: "Accept, Accept-Encoding, Accept-Language, Cookie",
+        },
+      });
+    }
 
     return new Response(JSON.stringify(categories), {
       headers: {
